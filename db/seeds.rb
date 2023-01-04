ad4ab0cfee77843ddc676df9de8fcde19bbb4f7c
Trummm <trungpro2000.tp2@gmail.com>
2023-01-04 17:07:03 +0700

    update4

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
 
-10.times do 
+20.times do 
   User.create(
-    
-
+    name: Faker::Artist.name,
+    email: Faker::Internet.email,
+    nickname: "@#{Faker::Internet.username}",
+    password: '123456',
+    isAdmin: false
   )
 end
